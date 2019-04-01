1b2a43584d172ddda7ff2b9a824032f358a756d6
zjx <1735429225@qq.com>
2019-04-02 00:05:02 +0800

    new

--- study1/pandas_learn.py
+++ study1/pandas_learn.py
@@ -9,16 +9,16 @@ import pandas as pd
 # df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list('ABCD'))
 # print(df)
 
-# df2 = pd.DataFrame({
-#     'A':1.,
-#     'B':pd.Timestamp('20130102'),
-#     'C':pd.Series(1,index=list(range(4)), dtype='float32'),
-#     'D':np.array([3]*4, dtype='int32'),
-#     'E':pd.Categorical(['test','train','test','train']),
-#     'F':'foo'
-# })
-# print(df2)
-# print(df2.dtypes)
+df2 = pd.DataFrame({
+    'A':1.,
+    'B':pd.Timestamp('20130102'),
+    'C':pd.Series(1,index=list(range(4)), dtype='float32'),
+    'D':np.array([3]*4, dtype='int32'),
+    'E':pd.Categorical(['test','train','test','train']),
+    'F':'foo'
+})
+print(df2)
+print(df2.dtypes)
 
 # print(df.head())
 # print(df.tail(3))
